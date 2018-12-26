6fd2abae2659894b36b226da2dcf2e34c3bdb798
praveen <praveen.mr400@gmail.com>
2018-12-26 13:46:27 +0530

    server

---
+++ build/precache-manifest.00b2994c4031a32ae3e59b981b746273.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "438fd0c6886400ce95e6",
+    "url": "/static/css/main.840c4d8b.chunk.css"
+  },
+  {
+    "revision": "438fd0c6886400ce95e6",
+    "url": "/static/js/main.438fd0c6.chunk.js"
+  },
+  {
+    "revision": "f281fb1a39eb8e2bf463",
+    "url": "/static/css/1.11015d33.chunk.css"
+  },
+  {
+    "revision": "f281fb1a39eb8e2bf463",
+    "url": "/static/js/1.f281fb1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0505cec1a1f87cc4ba7a11606c29089d",
+    "url": "/static/media/Logo.0505cec1.png"
+  },
+  {
+    "revision": "8cef14565fcab622a688cb403ddbbcee",
+    "url": "/static/media/404.8cef1456.gif"
+  },
+  {
+    "revision": "6db2122a54083108cd4cc936808bc14f",
+    "url": "/index.html"
+  }
+];
